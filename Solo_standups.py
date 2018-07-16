1f486cd4292dab9bfa29dcb0e63b777c21d5c687
kaipietila <kai.m.pietila@gmail.com>
2018-07-16 19:55:18 +0300

    added some docstrings and a cmd file

--- Solo_standups.py
+++ Solo_standups.py
@@ -1,6 +1,18 @@
 '''
-An app to record some plans on what to code the this session. 
-You can review your entries later via the app
+***SOLO STANDUP*** 
+by Kai Pietilä
+
+A program to record some plans on what to code the this session. 
+You can review your entries later via the program
+
+Name comes from when you at a comapny perform standups with your squad to see
+what needs to be done etc each morning. 
+Created this to have some nice log to follow my own coding and to keep some record for
+myself.
+
+Still needed features for full intended funtionality: 
+- Add TinyDB to be able to use the entries for more stuff, search etc. To be able 
+  to show an amount of entries on command.
 '''
 
 import datetime
@@ -8,28 +20,38 @@ import sys
 import time
 import os
 
-# getting the time from datetime
-
 todays_date = datetime.datetime.today()
 today_day = todays_date.day
 today_month = todays_date.month
 today_year = todays_date.year
-
-app_root = os.path.dirname(os.path.realpath(__file__))
+today_hour = todays_date.hour
+today_minute = todays_date.minute
+'''
+Setting up the date and time for the timestamp
+'''
+app_root = os.path.dirname(os.path.realpath(__file__)) 
+'''
+getting the path of the file to be able to create the .txt file in the right place
+'''
 
 class DailyEntry:
-    #class for creating a single entry
-
-    def __init__(self, day, month, year):
+    '''
+    Class for creating the User Entry
+    '''
+    def __init__(self, day, month, year, hour, minute):
 
         self.day = day
         self.month = month
         self.year = year
         self.entry = ''
+        self.hour = hour
+        self.minute = minute
     
     def prompt_for_entry(self):
-        #asking the user for input
-        print(f"Today is {self.day}.{self.month}.{self.year}")
+        '''
+        finc to ask the user for input. The astring is setup to be displayed in a nice flushing motion
+        '''
+        print(f"Date: {self.day}.{self.month}.{self.year}  Time: {self.hour}.{self.minute}")
         prompt_user = "What are you going to code this session? \n"
         for character in prompt_user:
             sys.stdout.write(character)
@@ -38,8 +60,10 @@ class DailyEntry:
         self.entry = input("> ")
 
     def save_to_file(self):
-        #saving to a file 
-        strdate = todays_date.strftime('%d/%m/%Y\n') #formats the date as a string
+        '''
+        Func to save the entry to the file and write the timestamp as string
+        '''
+        strdate = todays_date.strftime('%d %b %Y %H:%M\n') #formats the date as a string
         diary_file = open(app_root + "\diary_entries.txt", "a")
         diary_file.write(strdate)
         diary_file.write(self.entry)
@@ -48,7 +72,10 @@ class DailyEntry:
         print("ok, good luck!")
 
 def print_entries():
-    #if the user wishes to review old entries, he can print them out
+    '''
+    Func to print the entries written to the file. Basically just reading from the file the entries
+    if the user does not wish to read the entries the program exits
+    '''
     show_entries = "Do you want to read your previous entries? y/n\n"
     for character in show_entries:
         sys.stdout.write(character)
@@ -63,13 +90,18 @@ def print_entries():
         exit()
 
 def run_diary():
-    #main func to run the program
-    todays_entry = DailyEntry(today_day,today_month,today_year)
+    '''
+    Main func to run the program
+    '''
+    todays_entry = DailyEntry(today_day,today_month,today_year,today_hour, today_minute)
     todays_entry.prompt_for_entry()
     todays_entry.save_to_file()
     print_entries()
 
 
 if __name__ == "__main__":
+    '''
+    To run the program if main
+    '''
     run_diary()
 
